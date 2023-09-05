4a38412ee0d4c0719bb3dea3a5e746fabf28721f
Leonid Zulin <leo.zulin@gmail.com>
2023-09-05 22:16:50 +0300

    using annotation @Scope

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Component;
 @Component("personBean")
 public class Person {
 
-    @Autowired
-    @Qualifier("dog")// dog - бин по умолчанию
+    //@Autowired
+    //@Qualifier("dog")// dog - бин по умолчанию
     // @Qualifier("catBean") - нужен если несколько таких pet
     private Pet pet;
    // @Value("Zulin") - это хардкодовое написание
     // лучше через файл настроек
-    @Value("${person.surname}")
+    //@Value("${person.surname}")
     private String surname;
 
     // @Value("49") - - это хардкодовое написание
     // лучше через файл настроек
-    @Value("${person.age}")
+   // @Value("${person.age}")
     private int age;
 
     public String getSurname() {
---
+++ src/spring_introduction/ScopeTest.java
@@ -0,0 +1,18 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ScopeTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext31.xml");
+
+        Dog dog = context.getBean("dog", Dog.class);
+        Dog yourDog = context.getBean("dog", Dog.class);
+
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (dog == yourDog));
+        System.out.println(dog);
+        System.out.println(yourDog);
+        context.close();
+
+    }
+}
