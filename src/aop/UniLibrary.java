e6896cef8a92347c2b22712eba75b118522b5f16
Leonid Zulin <leo.zulin@gmail.com>
2023-09-18 17:54:23 +0300

    Proverka work Internet

--- src/aop/UniLibrary.java
+++ src/aop/UniLibrary.java
@@ -8,6 +8,7 @@ public class UniLibrary extends AbstractLibrary{
     // здесь рассмотрим метод, принимающий параметры
     public void getBook(Book book) {
         System.out.println("Мы берём книгу из библиотеки Университета: " + book.getName());
+        System.out.println("Мы берём книгу из библиотеки Университета: 2" + book.getName());
 
     }
 
