c288e7ceb37343d19ebdecb908437767b0eac49f
Leonid Zulin <leo.zulin@gmail.com>
2023-10-01 10:55:34 +0300

    Advice type @After

--- src/aop/aspects/UniversityLoggingAspect.java
+++ src/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -40,12 +37,16 @@ public class UniversityLoggingAspect {
 
     }*/
 
-    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+  /*  @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
     public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
         System.out.println("afterThrowingGetStudentsLoggingAdvice: логируем выброс исключения " + exception);
 
-    }
+    }*/
 
+    @After("execution(* getStudents())")
+    public void afterGetStudentLoggingAdvice() {
+        System.out.println("afterGetStudentLoggingAdvice: логируем нормальное окончание работы метода или выброс исключения");
+    }
 
 
 
